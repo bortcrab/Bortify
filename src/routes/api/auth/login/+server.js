0b14c02a73d22d79a0ce556314484edb752b451e
bortcrab <bortcrab@gmail.com>
2024-12-31 20:08:40 -0700

    Pulled project on Ubuntu

--- src/routes/api/auth/login/+server.js
+++ src/routes/api/auth/login/+server.js
@@ -2,8 +2,8 @@ import { json } from '@sveltejs/kit';
 
 export async function GET() {
     const client_id = import.meta.env.VITE_CLIENT_ID;
-    //const redirect_uri = 'http://localhost:5173/callback';
-    const redirect_uri = 'https://humble-space-meme-x4jjq9j6qqrfv54q-5173.app.github.dev/callback';
+    const redirect_uri = 'http://localhost:5173/callback';
+    //const redirect_uri = 'https://humble-space-meme-x4jjq9j6qqrfv54q-5173.app.github.dev/callback';
     const scope = 'user-read-private playlist-read-private';
 
     // Generates auth URL
